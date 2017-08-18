53876126ed9f52f9e9745da3a0d79dd4031ba43a
Travis <tscholl2@uw.edu>
2017-08-18 14:17:28 -0700

    remove training wheels, add todo to explain

--- src/rules/text.ts
+++ src/rules/text.ts
@@ -2,10 +2,12 @@ import { Rule } from "../parser";
 
 export default <Rule>{
   match: (s, { inline }) =>
-    // take at least one letter (that isn't a newline)
+    // This is taken from the text rule from simple-markdown.
+    // It takes at least one letter (that isn't a newline)
     // and keep going until we get to something that
     // might possibly match something else (image, emphasis, etc.)
-    // or the end of the match
+    // or the end of the match.
+    // TODO: explain this regexp
     inline ? /^[^\n]+?(?=[^0-9A-Za-z\s\u00c0-\uffff]|\n|$)/.exec(s) : null,
   parse: capture => ({ tag: "text", props: { content: capture[0] } }),
 };
