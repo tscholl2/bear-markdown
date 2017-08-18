901314dd6d03b86b7f6c3352f1bc2ca2db8fa2e3
Travis <tscholl2@uw.edu>
2017-08-18 15:21:02 -0700

    conistent-ize

--- src/rules/text.ts
+++ src/rules/text.ts
@@ -1,13 +1,14 @@
 import { Rule } from "../parser";
 
+// This is taken from the text rule from simple-markdown.
+// It takes at least one letter (that isn't a newline)
+// and keep going until we get to something that
+// might possibly match something else (image, emphasis, etc.)
+// or the end of the match.
+// TODO: explain this regexp and what \u00c0-\uffff is
+const re = /^[^\n]+?(?=[^0-9A-Za-z\s\u00c0-\uffff]|\n|$)/;
+
 export default <Rule>{
-  match: (s, { inline }) =>
-    // This is taken from the text rule from simple-markdown.
-    // It takes at least one letter (that isn't a newline)
-    // and keep going until we get to something that
-    // might possibly match something else (image, emphasis, etc.)
-    // or the end of the match.
-    // TODO: explain this regexp
-    inline ? /^[^\n]+?(?=[^0-9A-Za-z\s\u00c0-\uffff]|\n|$)/.exec(s) : null,
+  match: (s, { inline }) => (inline ? re.exec(s) : null),
   parse: capture => ({ type: "text", props: { content: capture[0] } }),
 };
