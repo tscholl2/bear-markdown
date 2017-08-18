a3b1231905e8e9cc9f0dda36afa05a065ae39b4c
Travis <tscholl2@uw.edu>
2017-08-18 15:54:12 -0700

    rebuild

--- lib/printers/html.d.ts
+++ lib/printers/html.d.ts
@@ -1,6 +1,6 @@
 import { Printer } from "../printer";
 import { Node } from "../parser";
 declare const _default: {
-    [type: string]: Printer<Node, any, string | Comment | HTMLElement>;
+    [type: string]: Printer<Node, any, string | HTMLElement | Comment>;
 };
 export default _default;
