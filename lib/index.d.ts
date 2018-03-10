23b0be366fa5c15e2b88ebe34858aaad3b4c55c9
Travis Scholl <tscholl2@uw.edu>
2018-03-09 18:06:50 -0800

    forgot to buidl

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,9 @@
-import { Node } from "./parser";
+import { newParser, Node } from "./parser";
+import { newPrinter } from "./printer";
+import { defaultRules } from "./rules";
 export declare const defaultParser: (source: string, state?: {}) => Node[];
 export declare const defaultHTMLPrinter: (tree: Node[], state?: {}) => any[];
-export { newParser, Rule, Node } from './parser';
+export { newParser, newPrinter, defaultRules };
+export { Rule, Node } from "./parser";
 export { newHTMLPrinters } from "./printers";
-export { Printer } from './printer';
-export { defaultRules } from './rules';
+export { Printer } from "./printer";
